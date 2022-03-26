942431c3c38572e83d759713f1074f419e53be14
dancevi <94169229+dancevi@users.noreply.github.com>
2022-03-26 13:18:29 +0200

    Final solution

--- fancy_barcodes.py
+++ fancy_barcodes.py
@@ -8,11 +8,19 @@ expression = re.compile(r"^@#+[A-Z][a-zA-Z0-9]{4,}[A-Z]@#+$")
 
 for _ in range(iterations):
     word = input()
-    my_list.append(word)
+    if expression.match(word):
+        product_group = ""
+        for letter in word:
+            if letter.isnumeric():
+                product_group += letter
+        if len(product_group) == 0:
+            product_group = "00"
+        print(f"Product group: {product_group}")
+    else:
+        print("Invalid barcode")
 
-filtered_list = [i for i in my_list if expression.match(i)]
 
-print(filtered_list)
+
 
 
 
